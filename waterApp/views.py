c7293fc3dfe6970a02de4af32ee36c61a9f891b0
Usman Daudu <adekunleakorede101@gmail.com>
2023-08-02 09:05:15 +0100

    Initial commit

---
+++ waterApp/views.py
@@ -0,0 +1,29 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import joblib
+
+def home(request):
+    return render(request,'home.html')
+
+def result(request):
+
+    pH_list = []
+    pH_list.append(request.GET['sulfate'])
+    pH_list.append(request.GET['hardness'])
+    pH_list.append(request.GET['solids'])
+
+    print(pH_list)
+
+    #pH_transformer = joblib.load('pH_transformer.sav')
+    #pH_scaler = joblib.load('pH_scaler.sav')
+    #pH_model = joblib.load('pH_model.sav')
+
+    #turb_transformer = joblib.load('turb_transformer.sav')
+    #turb_scaler = joblib.load('turb_scaler.sav')
+    #turb_model = joblib.load('turb_model.sav')
+
+    #main_transformer = joblib.load('transformer.sav')
+    #main_scaler = joblib.load('scaler.sav')
+    main_model = joblib.load('model.sav')
+
+    return render(request,'result.html')
