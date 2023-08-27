3c9c07ca4bbfddbf508573a70235b87e7efcdbeb
usmanadaudu <adekunleakorede101@gmail.com>
2023-08-27 01:19:57 +0100

    updating prediction processes with new inputs

--- waterApp/views.py
+++ waterApp/views.py
@@ -7,31 +7,41 @@ def home(request):
 
 def result(request):
 
-    pH_scaler = joblib.load('pH_scaler.sav')
     pH_model = joblib.load('pH_model.sav')
 
-    turb_scaler = joblib.load('turb_scaler.sav')
-    turb_model = joblib.load('turb_model.sav')
+    nitr_model = joblib.load('nitr_model.sav')
 
-    main_transformer = joblib.load('transformer.sav')
-    main_scaler = joblib.load('scaler.sav')
     main_model = joblib.load('model.sav')
 
     X = []
-    X.append(request.POST['sulfate'])
-    X.append(request.POST['hardness'])
-    X.append(request.POST['solids'])
+    X.append(request.POST['tempr'])
+    X.append(request.POST['do'])
+    X.append(request.POST['cond'])
+    X.append(request.POST['bod'])
+    X.append(request.POST['tclf'])
 
-    pH = pH_model.predict(pH_scaler.transform([X]))
+    pH = pH_model.predict([X])
 
+    X = []
+    X.append(request.POST['tempr'])
+    X.append(request.POST['do'])
     X.append(pH)
+    X.append(request.POST['cond'])
+    X.append(request.POST['bod'])
+    X.append(request.POST['tclf'])
 
-    turb = turb_model.predict(turb_scaler.transform([X]))
+    nitr = nitr_model.predict([X])
 
-    X.append(turb)
+    X = []
+    X.append(request.POST['tempr'])
+    X.append(request.POST['do'])
+    X.append(pH)
+    X.append(request.POST['cond'])
+    X.append(request.POST['bod'])
+    X.append(nitr)
+    X.append(request.POST['tclf'])
 
-    X_in = main_scaler.transform(main_transformer.transform([X]))
-    potability = main_model.predict(X_in)
+    potability = main_model.predict(X)
 
     if potability:
         pot = 'potable'
